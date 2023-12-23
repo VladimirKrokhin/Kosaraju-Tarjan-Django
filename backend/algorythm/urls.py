2b4cb942810788c6e95ebadeee3e0011bd639fc5
Vladimir Krokhin <krokhinvladimir2004@gmail.com>
2023-12-23 16:51:56 +0500

    class-based APIviews

--- backend/algorythm/urls.py
+++ backend/algorythm/urls.py
@@ -2,14 +2,14 @@ from django.urls import include, path
 from rest_framework import routers
 from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework.permissions import DjangoModelPermissions
-from . import views
+from algorythm import views
 
 
 app_name = "algorythm"
 urlpatterns = [
-    path('graphs/', views.graph_list),
-    path('graphs/<int:pk>/', views.graph_detail),
-    # path("create/", views.create_graph, name="create"),
+    path('graphs/', views.GraphList.as_view()),
+    path('graphs/<int:pk>/', views.GraphDetail.as_view()),
+    path("create/", views.create_graph, name="create"),
     path("index/<int:graph_id>/", views.index, name="index"),
 ]
 
--- backend/algorythm/views.py
+++ backend/algorythm/views.py
@@ -1,82 +1,88 @@
-from .models import Graph
+from algorythm.models import Graph
+from algorythm.serializers import GraphSerializer, GraphDetailSerializer
 
 from rest_framework import status
+from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.parsers import JSONParser
-from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.decorators import authentication_classes, permission_classes
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 
 
-from algorythm.serializers import GraphSerializer, GraphDetailSerializer
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required 
 from django.views.decorators.csrf import csrf_exempt
-from django.http import HttpResponse, JsonResponse
+from django.http import Http404
 
 
+class GraphList(APIView):
+        """
+        Отображает список графов пользователя или созает новый граф.
+        """
 
-
-@api_view(['GET', 'POST'])
-@authentication_classes([SessionAuthentication, BasicAuthentication])
-@permission_classes([IsAuthenticated])
-def graph_list(request, format=None):
-    """
-    Отображает список графов пользователя или создает новый граф.
-    """
-
-    user = request.user
-
-
-    if request.method == "GET":
-        try:
+        @authentication_classes([SessionAuthentication, BasicAuthentication])
+        @permission_classes([IsAuthenticated])
+        def get(self, request, format=None):
+            user = request.user
             graphs = Graph.objects.filter(user=user)
-        except Graph.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+            
+            if not graphs:
+                return Response(status=status.HTTP_404_NOT_FOUND)
 
-        serializer = GraphSerializer(graphs, many=True)
-        return Response(serializer.data)
-
-    elif request.method == 'POST':
-        serializer = GraphSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            serializer = GraphSerializer(graphs, many=True)
+            return Response(serializer.data)
 
 
+        @authentication_classes([SessionAuthentication, BasicAuthentication])
+        @permission_classes([IsAuthenticated])
+        def post(self, request, format=None):
+            serializer = GraphSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET', 'PUT', 'DELETE'])
-@authentication_classes([SessionAuthentication, BasicAuthentication])
-@permission_classes([IsAuthenticated])
-def graph_detail(request, pk, format=None):
+
+
+class GraphDetail(APIView):
     """
-    Получает, обновляет или удаляет граф.
+    Получает, обновляет или удаляет экземпляр графа.
     """
 
+    def get_object(self, pk, user):
+        try:
+            return Graph.objects.get(pk=pk, user=user)
+        except Snippet.DoesNotExist:
+            raise Http404
 
-    user = request.user
-    try:
-        graph = Graph.objects.get(pk=pk, user=user)
-    except Graph.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-
-    if request.method == 'GET':
+    
+    @authentication_classes([SessionAuthentication, BasicAuthentication])
+    @permission_classes([IsAuthenticated])
+    def get(self, request, pk, format=None):
+        graph = self.get_object(pk=pk, user=request.user)
         serializer = GraphDetailSerializer(graph)
         return Response(serializer.data)
 
-    elif request.method == 'PUT':
-        serializer = GraphSerializer(graph, data=request.data)
+
+    @authentication_classes([SessionAuthentication, BasicAuthentication])
+    @permission_classes([IsAuthenticated])
+    def put(self, request, pk, format=None):
+        graph = self.get_object(pk=pk, user=request.user)
+        serializer = GraphDetailSerializer(graph, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'DELETE':
+
+    @authentication_classes([SessionAuthentication, BasicAuthentication])
+    @permission_classes([IsAuthenticated])
+    def delete(self, request, pk, format=None):
+        graph = self.get_object(pk, user=request.user)
         graph.delete()
-        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 
     
@@ -90,17 +96,17 @@ def index(request, graph_id):
     else:
         return HttpResponseForbidden('You do not have permission to access this graph')
 
-# @login_required
-# def create_graph(request):
-#     if request.method == "POST":
-#         name = request.POST["name"]
-#         user = request.user
-#         graph = Graph.create(name, user)
+@login_required
+def create_graph(request):
+    if request.method == "POST":
+        name = request.POST["name"]
+        user = request.user
+        graph = Graph.create(name, user)
 
-#         return redirect(f'/graph/{graph.id}')
-#         # перенаправляем на страницу  созданного 
+        return redirect(f'/index/{graph.id}')
+        # перенаправляем на страницу  созданного 
 
-#     return render(request, "create_graph.html")
+    return render(request, "create_graph.html")
 
 
 
