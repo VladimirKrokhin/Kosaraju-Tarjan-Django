3a76674b5aa5ef3c7a1fc5d33c42299f790b9212
Vladimir Krokhin <krokhinvladimir2004@gmail.com>
2023-12-23 21:15:11 +0500

    Class generics api view

---
+++ backend/algorythm/permissions.py
@@ -0,0 +1,17 @@
+from rest_framework import permissions
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Кастомное разрешение, позволяющее только владельцам объектов редактировать их.
+    """
+
+
+    def has_object_permission(self, request, view, obj):
+        # Чтение разрешена любому запросу,
+        # следовательно разрешены GET, HEAD или 
+        # OPTIONS запросы.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        
+        # Разрешение на запись разрешены только владельцам
+        return obj.owner == request.user
--- backend/algorythm/views.py
+++ backend/algorythm/views.py
@@ -1,88 +1,72 @@
 from algorythm.models import Graph
-from algorythm.serializers import GraphSerializer, GraphDetailSerializer
+from algorythm.serializers import GraphSerializer, GraphDetailSerializer, UserSerializer
+from algorythm.permissions import IsOwnerOrReadOnly
 
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework.decorators import authentication_classes, permission_classes
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import generics, permissions, renderers
+from rest_framework.decorators import api_view
+from rest_framework.reverse import reverse
 
-
-from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required 
-from django.views.decorators.csrf import csrf_exempt
-from django.http import Http404
-
-
-class GraphList(APIView):
-        """
-        Отображает список графов пользователя или созает новый граф.
-        """
-
-        @authentication_classes([SessionAuthentication, BasicAuthentication])
-        @permission_classes([IsAuthenticated])
-        def get(self, request, format=None):
-            user = request.user
-            graphs = Graph.objects.filter(user=user)
-            
-            if not graphs:
-                return Response(status=status.HTTP_404_NOT_FOUND)
-
-            serializer = GraphSerializer(graphs, many=True)
-            return Response(serializer.data)
-
-
-        @authentication_classes([SessionAuthentication, BasicAuthentication])
-        @permission_classes([IsAuthenticated])
-        def post(self, request, format=None):
-            serializer = GraphSerializer(data=request.data)
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data, status=status.HTTP_201_CREATED)
-            return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+from django.shortcuts import get_object_or_404
 
 
 
-class GraphDetail(APIView):
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'graphs': reverse('graph-list', request=request, format=format)
+    })
+
+
+
+
+class GraphList(generics.ListCreateAPIView):
+    """
+    Отображает список графов пользователя или созает новый граф.
+    """
+    queryset =  Graph.objects.all()
+    serializer_class = GraphSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly]
+
+
+    # def perform_create(self, serializer):
+    #     serializer.save(owner=self.request.user)
+
+
+
+class GraphDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     Получает, обновляет или удаляет экземпляр графа.
     """
 
-    def get_object(self, pk, user):
-        try:
-            return Graph.objects.get(pk=pk, user=user)
-        except Snippet.DoesNotExist:
-            raise Http404
+    queryset =  Graph.objects.all()
+    serializer_class = GraphDetailSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly]
 
-    
-    @authentication_classes([SessionAuthentication, BasicAuthentication])
-    @permission_classes([IsAuthenticated])
-    def get(self, request, pk, format=None):
-        graph = self.get_object(pk=pk, user=request.user)
-        serializer = GraphDetailSerializer(graph)
-        return Response(serializer.data)
+    # def perform_create(self, serializer):
+    #     serializer.save(owner=self.request.user)
 
 
-    @authentication_classes([SessionAuthentication, BasicAuthentication])
-    @permission_classes([IsAuthenticated])
-    def put(self, request, pk, format=None):
-        graph = self.get_object(pk=pk, user=request.user)
-        serializer = GraphDetailSerializer(graph, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-    @authentication_classes([SessionAuthentication, BasicAuthentication])
-    @permission_classes([IsAuthenticated])
-    def delete(self, request, pk, format=None):
-        graph = self.get_object(pk, user=request.user)
-        graph.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
 
+
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 
     
@@ -90,36 +74,24 @@ class GraphDetail(APIView):
 def index(request, graph_id):
     user = request.user
     graph = get_object_or_404(Graph, pk=graph_id)
-    if (graph.user.id == user.id):
+    if (graph.owner == user):
         context = {"graph_id":graph_id}
         return render(request, 'index.html', context)
     else:
         return HttpResponseForbidden('You do not have permission to access this graph')
 
-@login_required
-def create_graph(request):
-    if request.method == "POST":
-        name = request.POST["name"]
-        user = request.user
-        graph = Graph.create(name, user)
-
-        return redirect(f'/index/{graph.id}')
-        # перенаправляем на страницу  созданного 
-
-    return render(request, "create_graph.html")
 
 
+# @login_required
+# def create_graph(request):
+#     if request.method == "POST":
+#         name = request.POST["name"]
+#         owner = request.user
+#         graph = Graph.create(name, owner)
 
-# class GraphViewSet(viewsets.ModelViewSet):
-#     """
-#     API эндпоинт который позволяет просматривать или редактировать граф.
-#     """
-#     queryset = Graph.objects.all()
-#     serializer_class = GraphSerializer
-#     permission_classes = [permissions.IsAuthenticated]
+#         return redirect(f'/index/{graph.id}')
+#         # перенаправляем на страницу  созданного 
 
-#     def list(self, request):
-#         user = request.user  # Получаем текущего пользователя
-#         graphs = Graph.objects.filter(user=user)  # Получаем все графы, принадлежащие этому пользователю
-#         serializer = GraphSerializer(graphs, context = {'request': request}, many=True)  # Сериализуем полученные графы
-#         return Response(serializer.data)
+#     return render(request, "create_graph.html")
+
+
